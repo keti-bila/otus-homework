5ab11fb4634ef20c45a9af786d9041c8ae494266
keti-bila <keti.bila@gmail.com>
2021-05-16 22:27:42 +0200

    Test for Yandex Market, refactored

--- src/test/java/HomeWork3Test.java
+++ src/test/java/HomeWork3Test.java
@@ -14,22 +14,16 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HomeWork3Test {
     private WebDriver driver;
     private WebDriverWait wait;
-    private Logger logger = LogManager.getLogger(HomeWork3Test.class);
-    private final String URL = "https://market.yandex.ru/";
+    private final Logger logger = LogManager.getLogger(HomeWork3Test.class);
+    private static final String URL = "https://market.yandex.ru/";
     private Actions actions;
-    String samsungName = null;
-    String xiaomiName = null;
-    private int sizeOfCompareList = 0;
-    List<WebElement> compareList = null;
-    String compareListXpath = "//div[@data-tid='412661c']";
-    boolean samsungIsPresent = false;
-    boolean xiaomiIsPresent = false;
 
     @BeforeTest
     public void setUp() {
@@ -43,7 +37,7 @@ public class HomeWork3Test {
     }
 
     @AfterTest
-    public void end() {
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
@@ -52,87 +46,67 @@ public class HomeWork3Test {
 
     @Test
     public void yandexTest() {
-//        Открыть в Chrome сайт Яндекс.Маркет - "Электроника"-> "Смартфоны"
+        String samsungBrand = "Samsung";
+        String xiaomiBrand = "Xiaomi";
         logger.info("Test started");
+        openMarketGoToSmartphones();
+        filterByBrandSortByPriceAscending(Arrays.asList(samsungBrand, xiaomiBrand));
+        String samsungName = addFirstPhoneOfBrandToCompareList(samsungBrand);
+        String xiaomiName = addFirstPhoneOfBrandToCompareList(xiaomiBrand);
+        driver.get(URL + "compare");
+        verifyCompareListHasAddedPositions(Arrays.asList(samsungName, xiaomiName));
+    }
+
+    private void openMarketGoToSmartphones() {
         driver.get(URL);
         WebElement buttonElektronika = driver.findElement(By.xpath("//span[contains(text(), 'Электроника')]"));
         buttonElektronika.click();
         WebElement buttonSmartfony = driver.findElement(By.xpath("//a[contains(@href, '/catalog--smartfony/')]"));
         buttonSmartfony.click();
         logger.info("Url is opened");
+    }
 
-//        Отфильтровать список товаров: Samsung и Xiaomi
-        WebElement checkboxSamsung = driver.findElement(By.xpath("//input[@name='Производитель Samsung']/following-sibling::div"));
-        WebElement checkboxXiaomi = driver.findElement(By.xpath("//input[@name='Производитель Xiaomi']/following-sibling::div"));
-        actions.moveToElement(checkboxSamsung).perform();
-        checkboxSamsung.click();
-        checkboxXiaomi.click();
+    private void filterByBrandSortByPriceAscending(List<String> brandNames) {
+        for (String brandName : brandNames) {
+            WebElement brandCheckbox = driver.findElement(By.xpath("//input[@name='Производитель " + brandName + "']/following-sibling::div"));
+            actions.moveToElement(brandCheckbox).perform();
+            brandCheckbox.click();
+        }
         By loaderLocator = By.xpath("//div[@data-tid='8bc8e36b']");
         waitForElementToBeGone(loaderLocator);
-
-//        Отсортировать список товаров по цене (от меньшей к большей)
         WebElement sortPrice = driver.findElement(By.xpath("//button[@data-autotest-id='dprice']"));
         sortPrice.click();
         waitForElementToBeGone(loaderLocator);
         logger.info("Phones are sorted by brand and price");
-
-//        Добавить первый в списке Samsung
-        WebElement firstSamsung = driver.findElement(By.xpath("(//a[contains(@title, 'Samsung')] )[1]"));
-        actions.moveToElement(firstSamsung).perform();
-        samsungName = driver.findElement(By.xpath("(//a[contains(@title, 'Samsung')] )[1]")).getAttribute("title");
-        WebElement addSamsung = driver.findElement(By.xpath("(//a[contains(@title, 'Samsung')] )[1]/parent::h3/parent::div/parent::div/preceding-sibling::div[2]/div[2]/div"));
-        addSamsung.click();
-        sizeOfCompareList++;
-
-//        Проверить, что отобразилась плашка "Товар {имя товара} добавлен к сравнению"
-        WebElement addedSamsung = driver.findElement(By.xpath("//*[contains(text(), 'Товар " + samsungName + " добавлен к сравнению')]"));
-        wait.until(ExpectedConditions.visibilityOf(addedSamsung));
-        logger.info("First Samsung is added");
-
-//        Добавить первый в списке Xiaomi
-        WebElement firstXiaomi = driver.findElement(By.xpath("(//a[contains(@title, 'Xiaomi')] )[1]"));
-        actions.moveToElement(firstXiaomi).perform();
-        xiaomiName = driver.findElement(By.xpath("(//a[contains(@title, 'Xiaomi')] )[1]")).getAttribute("title");
-        WebElement addXiaomi = driver.findElement(By.xpath("(//a[contains(@title, 'Xiaomi')] )[1]/parent::h3/parent::div/parent::div/preceding-sibling::div[2]/div[2]/div"));
-        addXiaomi.click();
-        sizeOfCompareList++;
-
-//        Проверить, что отобразилась плашка "Товар {имя товара} добавлен к сравнению"
-        WebElement addedXiaomi = driver.findElement(By.xpath("//*[contains(text(), 'Товар " + xiaomiName + " добавлен к сравнению')]"));
-        wait.until(ExpectedConditions.visibilityOf(addedXiaomi));
-        logger.info("First Xiaomi is added");
-
-//        Перейти в раздел Сравнение
-        driver.get("https://market.yandex.ru/compare");
-
-//        Проверить, что в списке товаров 2 позиции
-        compareList = driver.findElements(By.xpath(compareListXpath));
-        SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(compareList.size(), sizeOfCompareList, "Number of options in compare list is not as expected");
-        logger.info("Number of phones in compare list is " + compareList.size());
-        containString();
-        softAssert.assertTrue(samsungIsPresent, "Required Samsung is not in the list");
-        softAssert.assertTrue(xiaomiIsPresent, "Required Xiaomi is not in the list");
-        softAssert.assertAll();
-        logger.info("All phones are in compare list");
     }
 
-    private void containString() {
+    private String addFirstPhoneOfBrandToCompareList(String brandName) {
+        WebElement firstPhone = driver.findElement(By.xpath("(//a[contains(@title, '" + brandName + "')] )[1]"));
+        actions.moveToElement(firstPhone).perform();
+        String phoneName = driver.findElement(By.xpath("(//a[contains(@title, '" + brandName + "')] )[1]")).getAttribute("title");
+        WebElement addPhone = driver.findElement(By.xpath("(//a[contains(@title, '" + brandName + "')] )[1]/parent::h3/parent::div/parent::div/preceding-sibling::div[2]/div[2]/div"));
+        addPhone.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(), 'Товар " + phoneName + " добавлен к сравнению')]")));
+        logger.info("First {} is added", brandName);
+        return phoneName;
+    }
+
+    private void verifyCompareListHasAddedPositions(List<String> phoneNames) {
+        String compareListXpath = "//div[@data-tid='412661c']";
+        List<WebElement> compareList = driver.findElements(By.xpath(compareListXpath));
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(compareList.size(), 2, "Number of options in compare list is not as expected");
+        logger.info("Number of phones in compare list is " + compareList.size());
         ArrayList<String> phoneList = new ArrayList<>();
         for (int i = 1; i <= compareList.size(); i++) {
             String phone = driver.findElement(By.xpath(compareListXpath + "[" + i + "]" + "/div/a")).getText();
             phoneList.add(phone);
         }
-        for (String phoneName : phoneList) {
-            if (samsungName.equals(phoneName)) {
-                samsungIsPresent = true;
-            }
+        for (String phoneName : phoneNames) {
+            softAssert.assertTrue(isInList(phoneList, phoneName), "Required " + phoneName + " is not in the list");
         }
-        for (String phoneName : phoneList) {
-            if (xiaomiName.equals(phoneName)) {
-                xiaomiIsPresent = true;
-            }
-        }
+        softAssert.assertAll();
+        logger.info("All phones are in compare list");
     }
 
     private boolean isElementDisplayed(By elementSelector) {
@@ -149,6 +123,15 @@ public class HomeWork3Test {
         if (isElementDisplayed(elementSelector)) {
            wait.until(ExpectedConditions.invisibilityOfElementLocated(elementSelector));
         }
+    }
+
+    private boolean isInList(ArrayList<String> list, String name) {
+        for (String phoneName : list) {
+            if (name.equals(phoneName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
